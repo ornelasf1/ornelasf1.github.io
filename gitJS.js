19b949765f62962b4b98558b5c64c76d8adad91a
ornelasf1 <ornelasf1@gmail.com>
2017-06-10 15:07:11 -0500

    Update #2 added jQuery
    6-10 3:06pm: toggleclass - fadeIn - timeout func

--- gitJS.js
+++ gitJS.js
@@ -1,17 +1,11 @@
-document.addEventListener ("DOMContentLoaded", pageFullyLoaded);
+$(document).ready(function(){
+	setTimeout(function(){
+		$(".introText").fadeIn("slow", function(){
+			$(".introText").toggleClass("table-hidden");
+		});
+	}, 3000);
+	setTimeout(function(){
+		$("#intro").fadeOut("slow");
+	}, 6000);
+});
 
-function pageFullyLoaded(){
-}
-
-function fade(element) {
-    var op = 1;  // initial opacity
-    var timer = setInterval(function () {
-        if (op <= 0.1){
-            clearInterval(timer);
-            element.style.display = 'none';
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= op * 0.1;
-    }, 50);
-}
