3c3808ee8d75236015f4ef514a106acabe23f4b8
ornelasf1 <ornelasf1@gmail.com>
2017-06-17 15:00:16 -0500

    Update added animations

--- gitJS.js
+++ gitJS.js
@@ -1,11 +1,46 @@
+var iter = 0;
+var baseid;
+
 $(document).ready(function(){
-	setTimeout(function(){
-		$(".introText").fadeIn("slow", function(){
-			$(".introText").toggleClass("table-hidden");
-		});
-	}, 3000);
-	setTimeout(function(){
-		$("#intro").fadeOut("slow");
-	}, 6000);
+	baseid = setInterval(welcomeAnim(), 1000);
+	setInterval(alert("interval"), 4000);
+	disappearBar();
 });
+
+function welcomeText(){
+	setTimeout(function(){
+		$("#welcome1").fadeIn("slow");
+	}, 1000);
+	setTimeout(function(){
+		$("#welcome1").fadeOut("slow");
+	}, 3000);
+}
+
+function disappearBar(){
+	setTimeout(function(){
+		$("#intro-bar").fadeOut("slow");
+	}, 6000);
+}
+
+function welcomeAnim(){
+	if(iter++ > 3){
+		clearInterval(baseid);
+		return;
+	}
+	var selector = "welcome" + iter;
+	var text = document.getElementById(selector);
+	var pos = -100;
+	var id = setInterval(frame, 5);
+	function frame(){
+		if(pos == 2000){
+			clearInterval(id);
+		}else if(pos > 300 && pos < 600){
+			pos+=1;
+			text.style.left = pos + "px";
+		}else{
+			pos+=10;
+			text.style.left = pos + "px";
+		}
+	}
+}
 
